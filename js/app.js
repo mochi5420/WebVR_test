f6c6f82c431baf6727d55ed9cf4f20d20558e9ad
Keisuke Mochida <k.mochida5420@gmail.com>
2017-04-16 00:23:06 +0900

    Revert "test"
    This reverts commit 49d53ac01c4f2c192ced8c28383f68a8e5743823.

--- js/app.js
+++ js/app.js
@@ -1,56 +1,36 @@
-//レンダラー生成
-var renderer = new THREE.WebGLRenderer({ antialias: true });
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(
+    75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-//シーン生成
-var scene = new THREE.Scene();
-
-//カメラ生成
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-//VR用コントローラ生成
 var controls = new THREE.VRControls(camera);
 controls.standing = true;
 
-//VR用エフェクト生成　stereo, distotion, etc...
 var effect = new THREE.VREffect(renderer);
 effect.setSize(window.innerWidth, window.innerHeight);
 
-//VRマネージャ生成
 var manager = new WebVRManager(renderer, effect);
 
 window.addEventListener('resize', onResize, true);
 window.addEventListener('vrdisplaypresentchange', onResize, true);
 
-
-var geometry = new THREE.BoxGeometry(2, 1, 3);
+var geometry = new THREE.BoxGeometry(1, 1, 1);
 var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 cube.position.set(0, 0, -5);
 scene.add(cube);
 
-//アニメーションループ
-var lastRender = 0;
-function animate(timestamp) {
-    var delta = Math.min(timestamp - lastRender, 500);
-    lastRender = timestamp;
-
-    cube.rotation.x += delta * 0.00015;
-    cube.rotation.y += delta * 0.00025;
-
-    // VRコントローラのupdate
+function render() {
+    cube.rotation.x += 0.1;
+    cube.rotation.y += 0.1;
+    requestAnimationFrame(render);
     controls.update();
-
-    // VRマネージャを通してシーンをレンダリング
-    manager.render(scene, camera, timestamp);
-
-    // アニメーションループ
-    requestAnimationFrame(animate);
+    // renderer.render(scene, camera);
+    manager.render(scene, camera);
 }
-
-// アニメーションの開始
-animate(performance ? performance.now() : Date.now());
+render();
 
 function onResize(e) {
     effect.setSize(window.innerWidth, window.innerHeight);
