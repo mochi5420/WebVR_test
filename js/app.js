752d2a285f9dce8e5bd212d58188ef519082eba1
Keisuke Mochida <k.mochida5420@gmail.com>
2017-04-16 00:48:56 +0900

    aaa

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ document.body.appendChild(renderer.domElement);     //これでHTMLに貼り付けっぽい
 var scene = new THREE.Scene();
 
 //カメラ生成
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 
 //VR用コントローラ生成
 var controls = new THREE.VRControls(camera);
@@ -23,8 +23,33 @@ var manager = new WebVRManager(renderer, effect);
 window.addEventListener('resize', onResize, true);
 window.addEventListener('vrdisplaypresentchange', onResize, true);
 
+/////////////////////////////////
+//      オブジェクト生成        //
+/////////////////////////////////
+
+// Skysphereの生成
+var skysphereLoader = new THREE.TextureLoader();
+skysphereLoader.load('resources/sky.png', onSkysphereTextureLoaded);
+function onSkysphereTextureLoaded(texture) {
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set(4, 4);
+
+    var geometry = new THREE.SphereGeometry(5000, 128, 128);
+    // var geometry = new THREE.BoxGeometry(1000, 1000, 1000);
+    var material = new THREE.MeshBasicMaterial({
+        map: texture,
+        color: 0xffffff,
+        side: THREE.BackSide
+    });
+
+    var skysphere = new THREE.Mesh(geometry, material);
+    skysphere.position.z = 0;
+    scene.add(skysphere);
+}
+
 var geometry = new THREE.BoxGeometry(2, 1, 3);
-var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
+var material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 var cube = new THREE.Mesh(geometry, material);
 cube.position.set(0, 0, -5);
 scene.add(cube);
