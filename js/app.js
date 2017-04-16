278c9c7a08c4725a6490f56bdfec346b9ba70493
Keisuke Mochida <k.mochida5420@gmail.com>
2017-04-16 21:37:30 +0900

    lightを設置

--- js/app.js
+++ js/app.js
@@ -1,24 +1,29 @@
-//レンダラー生成
+//  レンダラー生成
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 //document.body.appendChild(renderer.domElement);     //これでHTMLに貼り付けっぽい？
 document.getElementById('container').appendChild(renderer.domElement);
 
-//シーン生成
+//  シーン生成
 var scene = new THREE.Scene();
 
-//カメラ生成
+//  カメラ生成
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 15000);
 
-//VR用コントローラ生成
+//  光源生成
+var directionalLight = new THREE.DirectionalLight(0xffeedd);
+directionalLight.position.set(0, 0, 1).normalize();
+scene.add(directionalLight);
+
+//  VR用コントローラ生成
 var controls = new THREE.VRControls(camera);
 //controls.standing = true;
 
-//VR用エフェクト生成
+//  VR用エフェクト生成
 var effect = new THREE.VREffect(renderer);
 effect.setSize(window.innerWidth, window.innerHeight);
 
-//VRマネージャ生成
+//  VRマネージャ生成
 var manager = new WebVRManager(renderer, effect);
 
 window.addEventListener('resize', onResize, true);
@@ -27,7 +32,7 @@ window.addEventListener('vrdisplaypresentchange', onResize, true);
 
 ///////////  オブジェクト生成  ////////////
 
-//Skysphere
+//  Skysphere
 var skysphereLoader = new THREE.TextureLoader();
 skysphereLoader.load('resources/sky3.png', onSkysphereTextureLoaded);
 function onSkysphereTextureLoaded(texture) {
@@ -50,8 +55,8 @@ function onSkysphereTextureLoaded(texture) {
     scene.add(skysphere);
 }
 
-//Obj
-//.obj .mtl を読み込んでいる時の処理
+//  Obj
+//  .obj .mtl を読み込んでいる時の処理
 var onProgress = function (xhr) {
     if (xhr.lengthComputable) {
         var percentComplete = xhr.loaded / xhr.total * 100;
@@ -59,12 +64,12 @@ var onProgress = function (xhr) {
     }
 };
 
-//.obj .mtl が読み込めなかったときのエラー処理
+//  .obj .mtl が読み込めなかったときのエラー処理
 var onError = function (xhr) { };
 
 THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
 
-//宇宙ステーション
+//  宇宙ステーション
 var mtlLoader = new THREE.MTLLoader();
 mtlLoader.setPath('resources/station/');
 mtlLoader.load('station.mtl', function (materials) {
@@ -77,7 +82,7 @@ mtlLoader.load('station.mtl', function (materials) {
     objLoader.load('station.obj', function (object) {
 
         object.scale.set(5, 5, 5);
-        object.position.set(0, 0, 0);
+        object.position.set(0, 0, -10);
         scene.add(object);
 
     }, onProgress, onError);
@@ -91,7 +96,7 @@ mtlLoader.load('station.mtl', function (materials) {
 //scene.add(cube);
 
 
-//アニメーションループ
+//  アニメーションループ
 var lastRender = 0;
 function animate(timestamp) {
     var delta = Math.min(timestamp - lastRender, 500);
@@ -110,7 +115,7 @@ function animate(timestamp) {
     requestAnimationFrame(animate);
 }
 
-// アニメーションの開始
+//  アニメーションの開始
 animate(performance ? performance.now() : Date.now());
 
 function onResize(e) {
